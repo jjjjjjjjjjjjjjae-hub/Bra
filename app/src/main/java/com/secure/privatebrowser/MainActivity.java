95da4b1077599f40399c59cfc4762b3d7e653a97
jjjjjjjjjjjjjjae-hub <jjjjjjjjjjjjjjjae@gmail.com>
2026-05-23 23:30:03 +0500

    Браузер тікелей localhost:8080 мекенжайына қосылатын етілді

--- app/src/main/java/com/secure/privatebrowser/MainActivity.java
+++ app/src/main/java/com/secure/privatebrowser/MainActivity.java
@@ -1,7 +1,6 @@
 package com.secure.privatebrowser;
 
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -9,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
-    private static final String TARGET_SERVER = "http://127.0.0.1:8080"; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +19,10 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
 
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
-                if (url.contains(".kaz") || url.contains("target.kaz")) {
-                    view.loadUrl(TARGET_SERVER);
-                    return true;
-                }
-                return false; 
-            }
-        });
-        myWebView.loadUrl("http://target.kaz");
+        // Қарапайым WebViewClient: ешқандай бағыттаусыз, сілтемелерді ішінде ашады
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // Браузер ашыла сала тікелей localhost серверіңізді жүктейді
+        myWebView.loadUrl("http://localhost:8080/");
     }
 }
