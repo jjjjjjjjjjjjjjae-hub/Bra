e15c5df7a40f633e14b21f157052a8b7d5939999
jjjjjjjjjjjjjjae-hub <jjjjjjjjjjjjjjjae@gmail.com>
2026-05-24 03:14:35 +0500

    Рұқсаттар Manifest-ке қосылды, код баяғы тұрақты нұсқаға қайтарылды

--- app/src/main/java/com/secure/privatebrowser/MainActivity.java
+++ app/src/main/java/com/secure/privatebrowser/MainActivity.java
@@ -66,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Кірген кезде бір-ақ рет рұқсат сұрау функциясы
         askPermissionsOnce();
+        
+        // Баяғы сен айтқан 8080 бұрылысы
         myWebView.loadUrl("http://localhost:8080");
     }
 
